7ee5db0d78a4e19dcd9974030bae226a6f645a70
João Henrique Silva <joaohsm13@gmail.com>
2023-09-12 09:47:59 -0300

    Create Condicional2.s

---
+++ Condicional2.s
@@ -0,0 +1,73 @@
+;condicinal
+segment .data ;declarar dados
+
+mens db "Digite um caracter",10  ;declarar variavel
+tam equ $-mens ;tamanho da mens
+
+mens2 db "Nao esta no intervalo",10  ;declarar variavel
+tam2 equ $-mens2 ;tamanho da mens2
+
+mens3 db "Esta no intervalo",10  ;declarar variavel
+tam3 equ $-mens3 ;tamanho da mens3
+
+
+
+segment .bss ;dados nao inicializados
+
+buff resb 100;reserva 100 espacos para buff
+qrec resd 1 ;bytes recebidos
+
+
+
+segment .text; linhas de codigos
+global _start
+_start:
+
+
+;print mens
+mov eax,4 ;print
+mov ebx,1 ;fd tela
+mov ecx,mens ;ponteiro string
+mov edx,tam ;qdde caracteres
+int 80h
+
+
+;read
+mov eax,3 ;read
+mov ebx,0 ;fd do teclado
+mov ecx,buff ;destino(pont)
+mov edx,100 ;quantos maxima
+int 80h
+mov [qrec],eax ;recebida >=1
+
+
+
+;comparar
+cmp[buff],byte "a"
+jb then
+
+; entra
+cmp[buff],byte "z"
+ja then
+
+;entra
+mov eax,4 ;print
+mov ebx,1 ;fd tela
+mov ecx,mens3 ;ponteiro string
+mov edx,tam3 ;qdde caracteres
+int 80h
+jmp contin
+
+
+then:
+;esta fora
+mov eax,4 ;print
+mov ebx,1 ;fd tela
+mov ecx,mens2 ;ponteiro string
+mov edx,tam2 ;qdde caracteres
+int 80h
+
+contin:
+;continua
+mov eax,1 ;finalizar
+int 80h ;syscall
